5e120bc4eaa7cac0a5a6dfe42cdf4af947618ede
Bowen Truelove <botruelove2002@gmail.com>
2026-08-26 15:08:16 -0500

    updated SQL file with current schema

--- Sql/CreateDatabase.sql
+++ Sql/CreateDatabase.sql
@@ -1,21 +1,51 @@
-drop table if exists FileJob;
+CREATE TABLE Dataset
+(
+	Id BIGINT IDENTITY(1, 1) NOT NULL CONSTRAINT PK_Dataset PRIMARY KEY,
+	DatasetKey nvarchar(50) NOT NULL,
+	DisplayName nvarchar(50) NOT NULL,
+	SourcePath nvarchar(2048) NOT NULL,
+	FilePattern nvarchar(50) NOT NULL,
+	StabilityCheckSeconds int NOT NULL,
+	IsEnabled bit NOT NULL DEFAULT 1,
+	CreatedAt DATETIME2 CONSTRAINT DF_Dataset_CreatedAt DEFAULT SYSUTCDATETIME(),
+	UpdatedAt DATETIME2
+)
+
+-- mock data configuration
+insert into Dataset (
+	DatasetKey, 
+	DisplayName, 
+	SourcePath, 
+	FilePattern, 
+	StabilityCheckSeconds
+) VALUES (
+	'mock-data',
+	'Mock Data',
+	'C:\\Temp\\TestData\\mock_data',
+	'*.csv',
+	0
+);
+
+CREATE TABLE DatasetColumnMapping
+(
+	Id BIGINT IDENTITY(1, 1) NOT NULL CONSTRAINT PK_DatasetColMap PRIMARY KEY,
+	DatasetId BIGINT FOREIGN KEY REFERENCES Dataset(Id),
+	SourceColumnIndex int NOT NULL,
+	DestinationColumnName nvarchar(100) NOT NULL,
+	DestinationColumnIndex int NOT NULL,
+	TargetType nvarchar(50),
+	IsRequired bit NOT NULL DEFAULT 1
+)
 
 CREATE TABLE FileJob
 (
 	Id BIGINT IDENTITY(1,1) NOT NULL CONSTRAINT PK_FileJob PRIMARY KEY,
-	DatasetName nvarchar(200) NOT NULL,
+	DatasetId BIGINT FOREIGN KEY REFERENCES Dataset(Id),
 	FilePath nvarchar(2048) NOT NULL,
-	FileName nvarchar(200),
 	Status nvarchar(50),
+	AttemptCount int NOT NULL DEFAULT 0,
 	DiscoveredAt DATETIME2(7) CONSTRAINT DF_FileJob_DiscoveredAt DEFAULT SYSUTCDATETIME(),
 	StartedAt DATETIME2(7),
 	CompletedAt DATETIME2(7),
 	ErrorMessage nvarchar(2048)
 );
-
-
-
-select * from FileJob;
-
-
-delete from FileJob;
